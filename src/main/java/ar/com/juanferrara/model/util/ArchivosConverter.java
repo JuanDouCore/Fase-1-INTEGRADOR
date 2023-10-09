3f5330a47c1716809e02cd0deb0e0ba4538bc1f9
JuanDouCore <juanjoyqueco.8@hotmail.com>
2023-10-09 15:06:04 -0300

    Metodo convertirInputStreamToFile() de la clase ArchivosConverter

--- src/main/java/ar/com/juanferrara/model/util/ArchivosConverter.java
+++ src/main/java/ar/com/juanferrara/model/util/ArchivosConverter.java
@@ -1,4 +1,16 @@
 package ar.com.juanferrara.model.util;
 
+import java.io.*;
+
 public class ArchivosConverter {
+
+    public static void convertirInputStreamToFile(InputStream inputStream, File file) throws FileNotFoundException {
+        try(FileOutputStream archivoBinario = new FileOutputStream(file)) {
+            byte[] bytes = inputStream.readAllBytes();
+            archivoBinario.write(bytes);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
