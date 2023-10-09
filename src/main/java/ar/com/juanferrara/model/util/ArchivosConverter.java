55151c218d942801c81e217caf1d59f738d2e22a
JuanDouCore <juanjoyqueco.8@hotmail.com>
2023-10-09 15:07:16 -0300

    Documentacion con comentarios de la clase ArchivosConverter

--- src/main/java/ar/com/juanferrara/model/util/ArchivosConverter.java
+++ src/main/java/ar/com/juanferrara/model/util/ArchivosConverter.java
@@ -2,8 +2,18 @@ package ar.com.juanferrara.model.util;
 
 import java.io.*;
 
+/**
+ * Clase de utilidad para manejar archivos
+ */
 public class ArchivosConverter {
 
+    /**
+     * Metodo que en base al InputStream que recibe en bytes lo mapea y escribe
+     * sobre un File para convertirlo en el archivo con sus respectivos bytes
+     * @param inputStream
+     * @param file
+     * @throws FileNotFoundException
+     */
     public static void convertirInputStreamToFile(InputStream inputStream, File file) throws FileNotFoundException {
         try(FileOutputStream archivoBinario = new FileOutputStream(file)) {
             byte[] bytes = inputStream.readAllBytes();
