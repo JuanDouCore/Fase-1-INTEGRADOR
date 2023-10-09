9428ab2fe0fc93cbd6ba14e34da0f59f65f6328e
JuanDouCore <juanjoyqueco.8@hotmail.com>
2023-10-09 14:39:54 -0300

    Se agregaron comentarios de documentacion

--- src/main/java/ar/com/juanferrara/dao/impl/PeliculaDAOimp.java
+++ src/main/java/ar/com/juanferrara/dao/impl/PeliculaDAOimp.java
@@ -6,36 +6,73 @@ import ar.com.juanferrara.model.domain.Pelicula;
 
 import java.util.List;
 
+/**
+ * Clase que cumple la implementacion como DAO para
+ * la entidad Pelicula
+ */
 public class PeliculaDAOimp implements DAO<Pelicula, Integer>, MySQLImplement {
+
+    /**
+     * Busca una pelicula por el codigo especificado
+     * y la retorna
+     * @param key
+     * @return pelicula
+     */
     @Override
     public Pelicula buscar(Integer key) {
         return null;
     }
 
+    /**
+     * Inserta una pelicula
+     * @param entity
+     */
     @Override
     public void insertar(Pelicula entity) {
 
     }
 
+    /**
+     * Modifica una pelicula
+     * @param entity
+     */
     @Override
     public void modificar(Pelicula entity) {
 
     }
 
+    /**
+     * Elimina una pelicula por el codigo
+     * @param key
+     */
     @Override
     public void eliminar(Integer key) {
 
     }
 
+    /**
+     * Lista todas las peliculas
+     * @return
+     */
     @Override
     public List<Pelicula> listarTodos() {
         return null;
     }
 
+    /**
+     * Lista todas las peliculas filtradas por un titulo
+     * @param titulo
+     * @return peliculas
+     */
     public List<Pelicula> listarTodosPorTitulo(String titulo) {
         return null;
     }
 
+    /**
+     * Lista todas las peliculas filtradas por su genero
+     * @param genero
+     * @return peliculas
+     */
     public List<Pelicula> listarTodosPorGenero(String genero) {
         return null;
     }
