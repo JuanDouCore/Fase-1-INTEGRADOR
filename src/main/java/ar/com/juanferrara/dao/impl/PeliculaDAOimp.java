52108b8748468679b65d0b9cf858639ad535c081
JuanDouCore <juanjoyqueco.8@hotmail.com>
2023-10-09 14:35:33 -0300

    Definicion de la estrucutra de la clase PeliculaDAOimp

--- src/main/java/ar/com/juanferrara/dao/impl/PeliculaDAOimp.java
+++ src/main/java/ar/com/juanferrara/dao/impl/PeliculaDAOimp.java
@@ -1,4 +1,42 @@
 package ar.com.juanferrara.dao.impl;
 
-public class PeliculaDAOimp {
+import ar.com.juanferrara.dao.DAO;
+import ar.com.juanferrara.dao.MySQLImplement;
+import ar.com.juanferrara.model.domain.Pelicula;
+
+import java.util.List;
+
+public class PeliculaDAOimp implements DAO<Pelicula, Integer>, MySQLImplement {
+    @Override
+    public Pelicula buscar(Integer key) {
+        return null;
+    }
+
+    @Override
+    public void insertar(Pelicula entity) {
+
+    }
+
+    @Override
+    public void modificar(Pelicula entity) {
+
+    }
+
+    @Override
+    public void eliminar(Integer key) {
+
+    }
+
+    @Override
+    public List<Pelicula> listarTodos() {
+        return null;
+    }
+
+    public List<Pelicula> listarTodosPorTitulo(String titulo) {
+        return null;
+    }
+
+    public List<Pelicula> listarTodosPorGenero(String genero) {
+        return null;
+    }
 }
