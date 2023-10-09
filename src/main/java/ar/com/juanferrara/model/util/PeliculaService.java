21ca8d72d130a7984a3f2a7d4105f4fac4613992
JuanDouCore <juanjoyqueco.8@hotmail.com>
2023-10-09 17:53:45 -0300

    Implementaciòn de la capa de servicio para Pelicula

--- src/main/java/ar/com/juanferrara/model/util/PeliculaService.java
+++ src/main/java/ar/com/juanferrara/model/util/PeliculaService.java
@@ -1,4 +1,45 @@
 package ar.com.juanferrara.model.util;
 
+import ar.com.juanferrara.dao.impl.PeliculaDAOimp;
+import ar.com.juanferrara.model.domain.Pelicula;
+
+import java.util.List;
+
 public class PeliculaService {
+
+    private final PeliculaDAOimp peliculaDAOimp;
+
+    public PeliculaService() {
+        this.peliculaDAOimp = new PeliculaDAOimp();
+    }
+
+    public void crearPelicula(Pelicula pelicula) {
+        peliculaDAOimp.insertar(pelicula);
+    }
+
+    public void modificarPelicula(Pelicula pelicula) {
+        peliculaDAOimp.modificar(pelicula);
+    }
+
+    public void eliminarPelicula(Pelicula pelicula) {
+        peliculaDAOimp.eliminar(pelicula.getCodigo());
+    }
+
+    public Pelicula mostrarPelicula(int codigo) {
+        return peliculaDAOimp.buscar(codigo);
+    }
+
+    public List<Pelicula> listarTodasPeliculas() {
+        return peliculaDAOimp.listarTodos();
+    }
+
+    public List<Pelicula> buscarPorCriterio(String criterio, String valor) {
+        if(criterio.equals("TITULO"))
+            return peliculaDAOimp.listarTodosPorTitulo(valor);
+        else if(criterio.equals("GENERO"))
+            return peliculaDAOimp.listarTodosPorGenero(valor);
+
+        return null;
+    }
+
 }
