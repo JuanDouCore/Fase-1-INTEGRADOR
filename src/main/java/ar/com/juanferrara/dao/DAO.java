cbbfcfcc43605c1a6b5bdf948a214a3d70b8cc6d
JuanDouCore <juanjoyqueco.8@hotmail.com>
2023-10-09 14:23:09 -0300

    Estructura de la interfaz DAO

--- src/main/java/ar/com/juanferrara/dao/DAO.java
+++ src/main/java/ar/com/juanferrara/dao/DAO.java
@@ -1,4 +1,12 @@
 package ar.com.juanferrara.dao;
 
-public interface DAO {
+import java.util.List;
+
+public interface DAO <E, K>{
+
+    E buscar(K key);
+    void insertar(E entity);
+    void modificar(E entity);
+    void eliminar(K key);
+    List<E> listarTodos();
 }
