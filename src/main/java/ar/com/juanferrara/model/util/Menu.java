4fa2fb5482429c375f205901f531f0b35cb02e09
JuanDouCore <juanjoyqueco.8@hotmail.com>
2023-10-09 19:16:59 -0300

    Documentacion con comentarios de la clase Menu

--- src/main/java/ar/com/juanferrara/model/util/Menu.java
+++ src/main/java/ar/com/juanferrara/model/util/Menu.java
@@ -2,8 +2,15 @@ package ar.com.juanferrara.model.util;
 
 import java.util.Scanner;
 
+/**
+ * Clase encargada de las operaciones de
+ * menu de la consola
+ */
 public class Menu {
 
+    /**
+     * Metodo principal para inciar el menù
+     */
     public void lanzarMenuPrincipal() {
         mostrarMenuPrincipal();
         int opcion = 1;
@@ -45,26 +52,55 @@ public class Menu {
             }
         } while (opcion != 0);
     }
+
+    /**
+     * Metodo encargado de mostrar las opciones
+     * del menu principal
+     */
     private void mostrarMenuPrincipal() {
 
     }
+
+    /**
+     * Metodo encargado de lanzar y operar el menu
+     * de listar peliculas
+     */
     private void menuListarPeliculas() {
     }
 
+    /**
+     * Metodo encargado de lanzar y operar el menu
+     * de busqueda de peliculas
+     */
     private void menuBuscarPelicula() {
     }
 
+    /**
+     * Metodo encargado de lanzar y operar el menu
+     * para modificar una pelicula
+     */
     private void menuModificarPelicula() {
     }
 
+    /**
+     * Metodo encargado de lanzar y operar el menu
+     * para eliminar una pelicula
+     */
     private void menuEliminarPelicula() {
     }
 
+    /**
+     * Metodo encargado de lanzar y operar el menu
+     * para crear peliculas
+     */
     private void menuCrearPelicula() {
     }
 
 
-
+    /**
+     * Metodo de utilidad pora ahorrar escribir todo un sysout
+     * @param string
+     */
     private void println(String string) {
         System.out.println(string);
     }
